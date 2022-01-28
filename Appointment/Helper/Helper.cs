62a251a27cd333b8a46997610c23ef5c751707f6
Zhendong Tang <zhendongtt@gmail.com>
2022-01-28 18:22:51 -0500

    2-7. Helper Class

---
+++ Appointment/Helper/Helper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace Appointment.Helper
+{
+    public static class Helper
+    {
+        static readonly string Admin = "Admin";
+        static readonly string Patient = "Patient";
+        static readonly string Doctor = "Doctor";
+
+
+        public static List<SelectListItem> GetRolesForDropDown()
+        {
+            return new List<SelectListItem>
+            {
+                new SelectListItem { Value = Helper.Admin, Text = Helper.Admin },
+                new SelectListItem { Value = Helper.Patient, Text = Helper.Patient },
+                new SelectListItem { Value = Helper.Doctor, Text = Helper.Doctor },
+            };
+        }
+    }
+
+    
+}

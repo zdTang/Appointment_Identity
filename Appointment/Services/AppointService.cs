7dcf2f12ac8950fde2d8b04e25e9a949ed223b13
Zhendong Tang <zhendongtt@gmail.com>
2022-01-29 15:34:44 -0500

    3-3. Appointment Service Get Doctors Part 1

--- Appointment/Services/AppointService.cs
+++ Appointment/Services/AppointService.cs
@@ -1,6 +1,33 @@
-﻿namespace Appointment.Services
+﻿using Appointment.Models;
+using Appointment.Models.ViewModels;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Appointment.Services
 {
-    public class AppointService: IAppointmentService
+    public class AppointService : IAppointmentService
     {
+        readonly ApplicationDbContext _db;
+        public AppointService(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public List<DoctorVM> GetDoctorList()
+        {
+            // Using this pattern must import "Linq"
+            var doctors = (from user in _db.Users
+                           // "select" is cast or project
+                           select new DoctorVM
+                           {
+                               Id = user.Id,
+                               Name = user.Name
+                           }).ToList();
+            return doctors;
+        }
+
+        public List<PatientVM> GetPatientList()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
--- Appointment/Services/IAppointmentService.cs
+++ Appointment/Services/IAppointmentService.cs
@@ -1,7 +1,11 @@
-﻿namespace Appointment.Services
+﻿using Appointment.Models.ViewModels;
+using System.Collections.Generic;
+
+namespace Appointment.Services
 {
     public interface IAppointmentService
     {
-
+        public List<DoctorVM> GetDoctorList();
+        public List<PatientVM> GetPatientList();
     }
 }

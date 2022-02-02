62826be19a866c44934b83bb6dd33bb6ee51248e
Zhendong Tang <zhendongtt@gmail.com>
2022-02-02 15:50:32 -0500

    Partial modal view validation doesn't work, will work later

---
+++ Appointment/Models/ViewModels/AddAppointmentVM.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Appointment.Models.ViewModels
+{
+    public class AddAppointmentVM
+    {
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Patient { get; set; }
+        [Required]
+        public string Duration { get; set; }
+    }
+}

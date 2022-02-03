ed0887d65e47c14a145b2a695094097ebb9f6553
Zhendong Tang <zhendongtt@gmail.com>
2022-02-03 11:11:26 -0500

    3-11. Api Endpoint and Common Response-1
    Create generic CommonResponse. this is a normal trick to return API result

---
+++ Appointment/Models/ViewModels/CommonResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Appointment.Models.ViewModels
+{
+    public class CommonResponse<T>
+    {
+        public int status { get; set; }
+        public string message { get; set; }
+        public T dataEnum { get; set; }
+
+    }
+}
--- Appointment/wwwroot/js/script-custom-calendar.js
+++ Appointment/wwwroot/js/script-custom-calendar.js
@@ -13,19 +13,22 @@ function InitializeCalendar() {
 
 
         var calendarEl = document.getElementById('calendar');
-        var calendar = new FullCalendar.Calendar(calendarEl, {
-            initialView: 'dayGridMonth',
-            headerToolbar: {
-                left: 'prev,next,today',
-                center: 'title',
-                right: 'dayGridMonth,timeGridWeek,timeGridDay'
-            },
-            selectable: true,
-            editable: false,
-            select: function (event) {
-                onShowModal(event, null);
-            }
-        });
+        if (calendarEl != null) {
+            var calendar = new FullCalendar.Calendar(calendarEl, {
+                initialView: 'dayGridMonth',
+                headerToolbar: {
+                    left: 'prev,next,today',
+                    center: 'title',
+                    right: 'dayGridMonth,timeGridWeek,timeGridDay'
+                },
+                selectable: true,
+                editable: false,
+                select: function (event) {
+                    onShowModal(event, null);
+                }
+            });
+        }
+        
         calendar.render();
 
 

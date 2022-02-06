7b7fa8f28d0ac2aa6789fba000f55c2a48ae2e40
Zhendong Tang <zhendongtt@gmail.com>
2022-02-06 06:32:41 -0500

    4-2 2. Get Calendar Data API

--- Appointment/Controllers/API/AppointmentApiController.cs
+++ Appointment/Controllers/API/AppointmentApiController.cs
@@ -4,6 +4,7 @@ using Appointment.Utility;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
 using System.Security.Claims;
 
 namespace Appointment.Controllers.API
@@ -48,6 +49,42 @@ namespace Appointment.Controllers.API
                 commonResponse.message = e.Message;
                 commonResponse.status = Helper.failure_code;
             }
+            return Ok(commonResponse);
+        }
+
+        // when users login the system, whatever he is an doctor or patient, the server will
+        // know the role. Based on the Role, the system will give different reaction 
+
+
+        [HttpGet]
+        [Route("GetCalendarData")]
+        public IActionResult GetCalendarData(string doctorId)
+        {
+            var commonResponse=new CommonResponse<List<AppointmentVM>>(); 
+            try
+            {
+                if (role == Helper.Patient)
+                {
+                    commonResponse.dataEnum=_appointmentService.PatientsEventsById(loginUserId);
+                    commonResponse.status = Helper.success_code;
+                }
+                else if (role == Helper.Doctor)
+                {
+                    commonResponse.dataEnum = _appointmentService.DoctorsEventsById(loginUserId);
+                    commonResponse.status = Helper.success_code;
+                }
+                else // Administrator
+                {
+                    commonResponse.dataEnum = _appointmentService.DoctorsEventsById(doctorId);
+                    commonResponse.status = Helper.success_code;
+                }
+            }
+            catch(Exception e)
+            {
+                commonResponse.message = e.Message;
+                commonResponse.status = Helper.failure_code;
+            }
+
             return Ok(commonResponse);
         }
        
